436f10ed679d238aaf05581b845ccd9950c1ce8d
jhoninsuasti3 <jhoninsuasti@gmail.com>
2024-10-02 23:20:21 -0500

    feat: add api founds

--- modules/funds/app.py
+++ modules/funds/app.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter, FastAPI
 import os
-from .services import FundService
-from .schemas import SubscribeRequest, CancelSubscriptionRequest
+from services import FundService
+from schemas import SubscribeRequest, CancelSubscriptionRequest
 from mangum import Mangum
+from starlette.requests import Request
 
 app = FastAPI(
     debug=os.getenv("DEBUG", False),
@@ -10,24 +11,30 @@ app = FastAPI(
 )
 
 
-router = APIRouter()
+router = APIRouter(prefix="/founds")
 
 service = FundService()
 
 
 @router.post("/subscribe")
-async def subscribe(request: SubscribeRequest):
-    result = await service.subscribe(request)
+async def api_subscribe(request: Request, suscribe_body: SubscribeRequest):
+    result = await service.subscribe(suscribe_body)
     return {"message": result}
 
 
 @router.post("/cancel")
-async def cancel_subscription(request: CancelSubscriptionRequest):
-    result = await service.cancel_subscription(request)
+async def api_cancel_subscription(
+    request: Request, cancel_suscription_body: CancelSubscriptionRequest
+):
+    result = await service.cancel_subscription(cancel_suscription_body)
     return {"message": result}
 
 
 @router.get("/funds")
-async def get_funds():
+async def api_get_funds():
     funds = await service.get_funds()
     return {"funds": funds}
+
+
+app.include_router(router)
+handler = Mangum(app)
